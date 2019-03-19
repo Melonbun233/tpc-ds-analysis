e872539cd71c64703ef59fcbc3e01e37e8aa67b4
Zhuohang Zeng <zeng_zh@foxmail.com>
2019-03-19 15:07:56 -0700

    add more details to count operators'

--- explain/py/CountOperators.py
+++ explain/py/CountOperators.py
@@ -2,6 +2,8 @@
 #The result is saved to count_operators.json
 from parse import parse_explain;
 import json;
+import numpy as np;
+import matplotlib.pyplot as plt;
 
 #recursive helper function to extract all plans
 #This funciton takes a dictionary
@@ -13,45 +15,55 @@ def extract_plan(plan, result):
 			extract_plan(child, result);
 
 #helper function to add a node_type count
-def result_add(node_type, result):
+def result_add(node_type, result, data_flow):
 	if (node_type in result):
-		result[node_type] += 1;
+		result[node_type]["Count"] += 1;
+		result[node_type]["Data Flow"] += data_flow;
 	else:
-		result[node_type] = 1;
+		result[node_type] = {"Count": 1, "Data Flow": data_flow};
 
 
 # Store a plan's info into result
 def store_plan(plan, result):
 	node_type = plan["Node Type"];
+	data_flow = plan["Plan Rows"] * plan["Plan Width"];
+	types = result["Node Types"];
 	# store number of this node type
 	# scan node
 	if("Scan" in node_type):
-		if ("Scan" in result["sub"]):
-			result_add(node_type, result["sub"]["Scan"]);
+		if ("Scan" in types):
+			result_add(node_type, types["Scan"]["Scan Types"], data_flow);
 		else:
-			result["sub"]["Scan"] = {node_type: 1}
+			types["Scan"] = {"Data Flow": 0, "Count": 0, 
+			"Scan Types": {node_type: {"Count": 1, "Data Flow": data_flow}}};
+		types["Scan"]["Data Flow"] += data_flow;
+		types["Scan"]["Count"] += 1; 
 	# join node
 	elif("Join" in node_type):
 		join_type = plan["Join Type"];
-		if("Join" in result["sub"]):
-			if(join_type in result["sub"]["Join"]):
-				result_add(node_type, result["sub"]["Join"][join_type]);
+		if("Join" in types):
+			if(join_type in types["Join"]["Join Types"]):
+				result_add(node_type, types["Join"]["Join Types"][join_type], data_flow);
 			else:
-				result["sub"]["Join"][join_type] = {node_type: 1};
+				types["Join"]["Join Types"][join_type] = {node_type: {"Count": 1, "Data Flow": data_flow}};
 		else:
-			result["sub"]["Join"] = {join_type:{node_type: 1}};
+			types["Join"] = {"Data Flow":0, "Count": 0, 
+			"Join Types":{join_type:{node_type: {"Count": 1, "Data Flow": data_flow}}}};
+		types["Join"]["Data Flow"] += data_flow;
+		types["Join"]["Count"] += 1;
 
 	#else
 	else:
-		result_add(node_type, result["sub"]);
+		result_add(node_type, types, data_flow);
 	# Filter is not a node type, but it is a operator
 	if ("Filter" in plan):
-		result_add("Filter", result["sub"]);
+		result_add("Filter", types, data_flow);
 
-	result["total"] += 1;
+	result["Total Count"] += 1;
+	result["Total Data Flow"] += data_flow;
 
 
-result = {"sub":{}, "total":0};
+result = {"Total Data Flow": 0, "Total Count":0, "Node Types":{}};
 #data is a list contains 99 results from queries
 data = parse_explain();
 for i in range(99):
@@ -67,6 +79,25 @@ f.write(result_str);
 f.close();
 
 #draw graph
+objects = [];
+counts = [];
+data_flows = [];
+types = result["Node Types"]
+
+for key in types:
+	objects.append(key);
+	counts.append(types[key]["Count"]);
+	data_flows.append(types[key]["Data Flow"]);
+
+index = np.arange(len(objects));
+
+plt.bar(index, counts, color = "SkyBlue");
+plt.xlabel("Operators");
+plt.ylabel("No. of operators");
+plt.xticks(index + 0.35, objects, fontsize = 5, rotation = 30);
+plt.title("Number of different operators used in tpc-ds with scale factor of 1");
+plt.show();
+
 
 
 
